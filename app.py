7cc60283d7080443ea814639d296a50594387577
sudu24 <39620310+sudu24@users.noreply.github.com>
2019-05-04 16:00:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def send_mail(id):
     msg.attach(MIMEText(body, 'plain'))
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login(fromaddr, "s24u5j92u")
+    server.login(fromaddr, "password")
     text = msg.as_string()
     server.sendmail(fromaddr, toaddr, text)
     server.quit()
